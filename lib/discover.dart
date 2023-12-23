4c5f22b32ee93981691fa0759c5b319656a5bed5
Nicholas Chera <nac@chera.xyz>
2023-12-22 20:27:09 -0800

    Add top padding for better Android experience

--- lib/discover.dart
+++ lib/discover.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 class DiscoverPage extends StatelessWidget {
   const DiscoverPage({super.key});
 
-  @override Widget build(BuildContext context) {
-    return const Text("This is the discover page!");
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.only(top: MediaQuery.of(context).viewPadding.top),
+        child: const Text("This is the discover page!"));
   }
 }
--- lib/profile.dart
+++ lib/profile.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
-  @override Widget build(BuildContext context) {
-    return const Text("This is the profile page!");
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.only(top: MediaQuery.of(context).viewPadding.top),
+        child: const Text("This is the profile page!"));
   }
 }
