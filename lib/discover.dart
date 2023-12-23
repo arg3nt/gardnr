3302430bebd82762ecc8a4049215b99377a2a26a
Nicholas Chera <nac@chera.xyz>
2023-12-22 20:15:24 -0800

    Build initial page nav

---
+++ lib/discover.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class DiscoverPage extends StatelessWidget {
+  const DiscoverPage({super.key});
+
+  @override Widget build(BuildContext context) {
+    return const Text("This is the discover page!");
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:gardnr/discover.dart';
+import 'package:gardnr/messages.dart';
+import 'package:gardnr/profile.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Gardnr',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -56,6 +60,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  int _page = 0;
 
   void _incrementCounter() {
     setState(() {
@@ -68,8 +73,20 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _setPage(int newPage) {
+    setState(() {
+      _page = newPage;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    var pageContent = switch (_page) {
+      0 => DiscoverPage(),
+      1 => MessagesPage(),
+      2 => ProfilePage(),
+      _ => const Text("yikes")
+    };
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -77,49 +94,26 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _page,
+          onDestinationSelected: _setPage,
+          destinations: [
+            NavigationDestination(
+                icon: SvgPicture.asset('images/discoverIcon.svg',
+                    height: 30,
+                    colorFilter: ColorFilter.mode(
+                        Theme.of(context).primaryColor, BlendMode.srcIn)),
+                label: "Discover"),
+                NavigationDestination(icon: SvgPicture.asset('images/messageIcon.svg',
+                      height: 30,
+                      colorFilter: ColorFilter.mode(
+                          Theme.of(context).primaryColor, BlendMode.srcIn)), label: "Messages"),
+            NavigationDestination(icon: SvgPicture.asset('images/userIcon.svg',
+                      height: 30,
+                      colorFilter: ColorFilter.mode(
+                          Theme.of(context).primaryColor, BlendMode.srcIn)), label: "Profile")
+          ]),
+      body: pageContent, // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
