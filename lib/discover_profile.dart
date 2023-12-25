0950d6dc5c0210c7a5fe43c9cf30fa935bc37fda
Nicholas Chera <nac@chera.xyz>
2023-12-24 18:41:20 -0800

    Carousel supports no images and single image better.

--- lib/discover_profile.dart
+++ lib/discover_profile.dart
@@ -33,34 +33,47 @@ class DiscoverProfileState extends State<DiscoverProfile> {
 
   @override
   Widget build(BuildContext context) {
-    var imgDesc = widget.profile.gardenerProfile!.images[_carouselIndex];
+    List<Widget> imageStackChildren = [];
+    if (widget.profile.gardenerProfile!.images.isNotEmpty) {
+      var imgDesc = widget.profile.gardenerProfile!.images[_carouselIndex];
+      imageStackChildren.add(Image(
+          image: switch (imgDesc.source) {
+            ImageSource.asset => AssetImage(imgDesc.path,
+                bundle: widget.profile.gardenerProfile!.assets),
+            ImageSource.file => FileImage(File(imgDesc.path)),
+          } as ImageProvider<Object>,
+          fit: BoxFit.fitHeight,
+          height: double.infinity,
+          width: double.infinity));
+
+      // Add carousel buttons if necessary.
+      if (widget.profile.gardenerProfile!.images.length > 1) {
+        imageStackChildren.add(Row(
+          children: [
+            IconButton(
+                iconSize: 40,
+                onPressed: () => {retreatCarousel()},
+                icon: const Icon(Icons.chevron_left, color: Colors.white)),
+            const Spacer(flex: 1),
+            IconButton(
+                iconSize: 40,
+                onPressed: () => {advanceCarousel()},
+                icon: const Icon(Icons.chevron_right, color: Colors.white))
+          ],
+        ));
+      }
+    } else {
+      imageStackChildren.add(Image.asset('images/img_not_found.jpg',
+          bundle: widget.profile.gardenerProfile!.assets,
+          fit: BoxFit.fitHeight,
+          height: double.infinity,
+          width: double.infinity));
+    }
+
     return Stack(
       alignment: AlignmentDirectional.bottomStart,
       children: [
-        Stack(alignment: Alignment.center, children: [
-          Image(
-              image: switch (imgDesc.source) {
-                ImageSource.asset => AssetImage(imgDesc.path,
-                    bundle: widget.profile.gardenerProfile!.assets),
-                ImageSource.file => FileImage(File(imgDesc.path)),
-              } as ImageProvider<Object>,
-              fit: BoxFit.fitHeight,
-              height: double.infinity,
-              width: double.infinity),
-          Row(
-            children: [
-              IconButton(
-                  iconSize: 40,
-                  onPressed: () => {retreatCarousel()},
-                  icon: const Icon(Icons.chevron_left, color: Colors.white)),
-              const Spacer(flex: 1),
-              IconButton(
-                  iconSize: 40,
-                  onPressed: () => {advanceCarousel()},
-                  icon: const Icon(Icons.chevron_right, color: Colors.white))
-            ],
-          )
-        ]),
+        Stack(alignment: Alignment.center, children: imageStackChildren),
         Container(
             padding:
                 const EdgeInsets.only(top: 75, bottom: 10, left: 10, right: 10),
